d75fe4735af50af4f86b6ade8bceb2d38cada277
Wilber Torres <winftc@gmail.com>
2017-11-04 01:35:11 -0500

    java formatting

--- src/test/java/org/patterns/creational/builder/CarTest.java
+++ src/test/java/org/patterns/creational/builder/CarTest.java
@@ -12,10 +12,7 @@ public class CarTest {
     int wheels = 4;
     String color = "Red";
 
-    Car car = new Car.Builder(color)
-        .size(Size.SMALL)
-        .wheels(wheels)
-        .build();
+    Car car = new Car.Builder(color).size(Size.SMALL).wheels(wheels).build();
 
     Assert.assertEquals(color, car.getColor());
     Assert.assertEquals(Size.SMALL, car.getSize());
